8839aaca051f7c8f6450d3cacd6cf8667c643f96
Rishad Bharucha <rb2102@gmail.com>
2016-03-19 21:20:10 -0600

    Restructure app and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 "use strict";
 var del = require('del'),
     gulp = require('gulp'),
+    cache = require('gulp-cache'),
     concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
     nunjucksRender = require('gulp-nunjucks-render'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
@@ -10,36 +12,46 @@ var del = require('del'),
 
     input = {
         'sass': [
-            'assets/sass/main.scss'
+            'app/assets/sass/main.scss'
         ],
         'javascript': [
-            'assets/js/jquery.min.js',
-            'assets/js/jquery.dropotron.min.js',
-            'assets/js/skel.min.js',
-            'assets/js/util.js',
-            'assets/js/main.js'
-        ]
+            'app/assets/js/jquery.min.js',
+            'app/assets/js/jquery.dropotron.min.js',
+            'app/assets/js/skel.min.js',
+            'app/assets/js/util.js',
+            'app/assets/js/main.js'
+        ],
+        'images': 'app/assets/images/**/*.+(png|jpg|jpeg|gif|svg)',
+        'fonts': 'app/assets/fonts/**/*',
+        'fontawesome': 'app/assets/css/font-awesome.min.css',
+        'pages': 'app/pages/*.nunjucks',
+        'templates': 'app/templates/'
     },
 
     output = {
-        'javascript': 'assets/js',
-        'stylesheets': 'assets/css',
-        'html': '*.html'
-    };
+        'pages': 'dist',
+        'images': 'dist/assets/images',
+        'javascript': 'dist/assets/js',
+        'stylesheets': 'dist/assets/css',
+        'fonts': 'dist/assets/fonts',
+        'html': 'dist/*.html'
+    },
+
+    clean = 'dist';
 
 // Run the watch task when gulp is called without arguments
 gulp.task('default', ['clean', 'build', 'watch', 'browser-sync']);
 
 // Clean the build directory
 gulp.task('clean', function() {
-    return del(['build']);
+    return del([clean]);
 });
 
 // Build assets
-gulp.task('build', ['build-css', 'build-js']);
+gulp.task('build', ['images', 'sass', 'js', 'pages', 'fonts', 'fontawesome']);
 
 // Compile SCSS files
-gulp.task('build-css', function() {
+gulp.task('sass', function() {
     return gulp.src(input.sass)
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -51,7 +63,7 @@ gulp.task('build-css', function() {
 });
 
 // Concat JavaScript files
-gulp.task('build-js', function() {
+gulp.task('js', function() {
     return gulp.src(input.javascript)
         .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
@@ -59,15 +71,34 @@ gulp.task('build-js', function() {
         .pipe(gulp.dest(output.javascript));
 });
 
+gulp.task('images', function() {
+    return gulp.src(input.images)
+        // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest(output.images))
+});
+
 // Use nunjucks templating engine
-gulp.task('nunjucks', function() {
-    nunjucksRender.nunjucks.configure(['app/templates/']);
-    // Gets .html and .nunjucks files in pages
-    return gulp.src('app/pages/**/*.+(html|nunjucks)')
-        // Renders template with nunjucks
-        .pipe(nunjucksRender())
-        // output files in root folder
-        .pipe(gulp.dest('.'))
+gulp.task('pages', function() {
+    return gulp.src(input.pages)
+        .pipe(nunjucksRender({
+            path: [input.templates]
+        }))
+        .pipe(gulp.dest(output.pages));
+});
+
+// Copy fonts to production folder
+gulp.task('fonts', function() {
+    return gulp.src(input.fonts)
+        .pipe(gulp.dest(output.fonts));
+});
+
+// Copy fonts to production folder
+gulp.task('fontawesome', function() {
+    return gulp.src(input.fontawesome)
+        .pipe(gulp.dest(output.stylesheets));
 });
 
 // Refresh the browser when changes are made to the watch files
@@ -82,6 +113,7 @@ gulp.task('browser-sync', function() {
 
 // Watch these files for changes and run the task on update
 gulp.task('watch', function() {
-    gulp.watch(input.javascript, ['build-js']);
-    gulp.watch(input.sass, ['build-css']);
+    gulp.watch(input.sass, ['sass']);
+    gulp.watch(input.javascript, ['js']);
+    gulp.watch(input.sass, ['pages']);
 });
