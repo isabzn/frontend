956e20f976e30e0ac45ff4c5498114f464c8080a
Rishad Bharucha <rb2102@gmail.com>
2016-03-20 15:02:15 -0600

    Fix current li class dynamic switching

--- app/assets/js/nav-active.js
+++ app/assets/js/nav-active.js
@@ -1,14 +1,14 @@
+// Dynamically add the "current" class to the list element in the navigation menu
 $(function() {
     var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
     $("#nav ul li a").each(function() {
         if ($(this).attr("href") == pgurl) {
             $(this).parent().addClass("current");
-        }
-        else if (($(this).attr("href") == 'current-students.html') || ($(this).attr("href") == 'prospective-students.html')) {
-            
-        }
-        else if ($(this).attr("href") == 'india-night.html') {
-
+            if (('current-students.html' == pgurl) || ('prospective-students.html' == pgurl)) {
+                $("nav ul").find("#students").addClass("current");
+            } else if ('india-night.html' == pgurl) {
+                $("nav ul").find("#events").addClass("current");
+            }
         }
     })
 });
